d7e52cd90b91165bf92046552a59c3e12a94d808
Jerrad <corso77@live.com>
2018-02-03 22:14:24 -0700

    middle of sub list code

--- app/src/main/java/com/example/coursauxsubbook/addSubscription.java
+++ app/src/main/java/com/example/coursauxsubbook/addSubscription.java
@@ -1,13 +1,44 @@
 package com.example.coursauxsubbook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class addSubscription extends AppCompatActivity {
+    private static Subscription newSubscription;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_subscription);
+    }
+
+    public void ChangePage(View view) throws ParseException {
+        /* from https://stackoverflow.com/questions/8498880/retrieving-data-from-edittext-in-android*/
+        EditText editText = (EditText) findViewById(R.id.NameFill);
+        String name = editText.getText().toString();
+
+        EditText editText1 = (EditText) findViewById(R.id.DateFill);
+        String dateString = editText1.getText().toString();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("DD/MM/YYYY");
+        Date date = dateFormat.parse(dateString);
+
+        EditText editText2 = (EditText) findViewById(R.id.PriceFill);
+        float price = Float.valueOf(editText2.getText().toString());
+
+        newSubscription = new Subscription(name, date, price);
+
+        Intent intent = new Intent(this, subscriptionPage.class);
+        startActivity(intent);
+    }
+
+    public static Subscription getNewSubscription() {
+        return newSubscription;
     }
 }
--- app/src/main/java/com/example/coursauxsubbook/subscriptionPage.java
+++ app/src/main/java/com/example/coursauxsubbook/subscriptionPage.java
@@ -1,5 +1,6 @@
 package com.example.coursauxsubbook;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,18 +10,30 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.gson.Gson;
+
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class subscriptionPage extends AppCompatActivity {
 
     private static final String FILENAME = "newfile.sav";
-    private EditText bodyText;
     private ListView SubscriptionListView;
-    private ArrayList<Subscription> subscriptionlist;
+    private static ArrayList<Subscription> subscriptionlist;
     private ArrayAdapter<Subscription> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        try{
+            subscriptionlist.add(addSubscription.getNewSubscription());
+            adapter.notifyDataSetChanged();
+
+            saveInFile();
+        }catch (NullPointerException e){}
         super.onCreate(savedInstanceState);
         setContentView(R.layout.subscription_page);
 
@@ -38,5 +51,26 @@ public class subscriptionPage extends AppCompatActivity {
     public void ChangePage(View view) {
         Intent intent = new Intent(this, addSubscription.class);
         startActivity(intent);
+    }
+
+    private void saveInFile() {
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME,
+                    Context.MODE_PRIVATE);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+
+            Gson gson = new Gson();
+
+            gson.toJson(subscriptionlist, out);
+
+            out.flush();
+
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
