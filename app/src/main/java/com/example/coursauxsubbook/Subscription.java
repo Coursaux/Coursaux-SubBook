6099e2cd56fafa807352c44ce30b5cba4f14ac54
Jerrad <corso77@live.com>
2018-02-05 13:30:02 -0700

    click listview not working

--- app/src/main/java/com/example/coursauxsubbook/Subscription.java
+++ app/src/main/java/com/example/coursauxsubbook/Subscription.java
@@ -6,18 +6,23 @@ import java.util.Date;
  * Created by Jerrad on 2018-01-27.
  */
 
+/**
+ * represents a subscription
+ */
 public class Subscription {
     private String name;
     private Date date;
     private Float price;
     private String comment;
 
-    Subscription( String name, Date date, Float price){
-        this.name = name;
-        this.date = date;
-        this.price = price;
-    }
-
+    /**
+     * initializes with name, date, price, and comment
+     *
+     * @param name - subscription name
+     * @param date - subscription date
+     * @param price - subscription price
+     * @param comment - subscription comment
+     */
     Subscription( String name, Date date, Float price, String comment){
         this.name = name;
         this.date = date;
@@ -25,30 +30,65 @@ public class Subscription {
         this.comment = comment;
     }
 
+    /**
+     * to change the name of the subscription
+     *
+     * @param name - the new name
+     */
     public void setName(String name){
         this.name = name;
     }
 
+    /**
+     * to change the price of the subscription
+     *
+     * @param price - the new price
+     */
     public void setPrice(Float price) {
         this.price = price;
     }
 
+    /**
+     * to change the comment on the subscription
+     *
+     * @param comment - the new comment
+     */
     public void setComment(String comment) {
         this.comment = comment;
     }
 
+    /**
+     * returns the name of the subscription
+     *
+     * @return - subscription name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * returns the date of the subscription
+     *
+     * @return - date
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     * returns the price of the subscription
+     *
+     * @return - price
+     */
     public Float getPrice() {
         return price;
     }
 
+    /**
+     * returns the comment
+     *
+     * @return - comment
+     */
     public String getComment() {
         return comment;
     }
--- app/src/main/java/com/example/coursauxsubbook/subscriptionPage.java
+++ app/src/main/java/com/example/coursauxsubbook/subscriptionPage.java
@@ -1,50 +1,97 @@
+/**
+ * Issues:
+ *      1. the way I had to update the listview prevents it from saving after closing, any other
+ *      way of doing it caused the app to crash.
+ *      2. the setOnItemClickListener didn't work no matter what I tried. multiple hours were
+ *      trying to fix it it to no avail.
+ *      3. The emulator froze my computer at one point and I lost the tabs I had open, so some
+ *      parts aren't cited properly.
+ *
+ */
+
 package com.example.coursauxsubbook;
 
-import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
-import com.google.gson.Gson;
-
-import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
-public class subscriptionPage extends AppCompatActivity {
+/**
+ * the main activity
+ */
+public class subscriptionPage extends AppCompatActivity  {
 
     private static final String FILENAME = "newfile.sav";
     private ListView lv;
     private ArrayList<String> subList = new ArrayList<String>();
+    private ArrayList<Subscription> subListR = new ArrayList<Subscription>();
     private ArrayAdapter<Subscription> adapter;
 
+    /**
+     * creating the instance
+     *
+     * @param savedInstanceState
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.subscription_page);
+
+        lv = (ListView) findViewById(R.id.SubListView);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            /**
+             * changes to the edit subscription page and fills the edittexts
+             * with info from the subscription to be edited
+             *
+             * @param parent
+             * @param view
+             * @param i
+             * @param id
+             */
+            /* from stackoverflow, lost url */
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                Log.i("tag1", "it works!");
+                setContentView(R.layout.sub_info);
+                /* from stackoverflow, lost url */
+                EditText et = (EditText) findViewById(R.id.NameEdit);
+                et.setText(subListR.get(i).getName());
+                EditText et1 = (EditText) findViewById(R.id.DateEdit);
+                et1.setText(subListR.get(i).getDate().toString());
+                EditText et2 = (EditText) findViewById(R.id.PriceEdit);
+                et2.setText(subListR.get(i).getPrice().toString());
+            }
+        });
     }
 
-    @Override
-    protected void onStart(){
-        super.onStart();
-    }
 
+    /**
+     * called when clicking on the add subscription button, changes to a new layout
+     *
+     * @param view
+     */
     public void ChangePage(View view) {
         setContentView(R.layout.activity_add_subscription);
     }
 
+    /**
+     * called when clicking on the add button in the add subscription layout
+     * adds a subscription to the list and list view
+     *
+     * @param view
+     * @throws ParseException
+     */
     public void ChangePageBack(View view) throws ParseException {
          /* from https://stackoverflow.com/questions/8498880/retrieving-data-from-edittext-in-android*/
         EditText editText = (EditText) findViewById(R.id.NameFill);
@@ -52,19 +99,31 @@ public class subscriptionPage extends AppCompatActivity {
 
         EditText editText1 = (EditText) findViewById(R.id.DateFill);
         String dateString = editText1.getText().toString();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("DD/MM/YYYY");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("YYYY-MM-DD");
         Date date = dateFormat.parse(dateString);
 
         EditText editText2 = (EditText) findViewById(R.id.PriceFill);
         float price = Float.valueOf(editText2.getText().toString());
 
-        Subscription sub = new Subscription(name, date, price);
+        EditText editText3 = (EditText) findViewById(R.id.NameFill);
+        String comment = editText3.getText().toString();
+
+        Subscription sub = new Subscription(name, date, price, comment);
 
         setContentView(R.layout.subscription_page);
 
         lv = (ListView) findViewById(R.id.SubListView);
 
+        subListR.add(sub);
         subList.add(sub.getName());
+
+        float k = 0;    /* Here until issue with click listener solved*/
+        for(int i=0; i<subListR.size(); i++){
+            k = k + subListR.get(i).getPrice();
+        }
+        String l = String.format("%.2f", k);
+        TextView tv = (TextView) findViewById(R.id.PriceTotal);
+        tv.setText(l);  /* Here until issue with click listener solved*/
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
                 this,
@@ -73,4 +132,5 @@ public class subscriptionPage extends AppCompatActivity {
 
         lv.setAdapter(arrayAdapter);
     }
+
 }
